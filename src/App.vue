481714aa8c6d11b0ba2638fe027e460d232e5b4c
Alyona Malinina <alenam2803@mail.ru>
2020-06-21 18:21:39 +0300

    added item

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,102 @@
 <template>
-  <div id="app">
-    <tabs ref="tabs" @indexChanged="indexChangedHandler">
-      <tab name="Корпоративные ценности" :selected="true">
-        <h1>Корпоративные ценности</h1>
-      </tab>
-      <tab name="Корпоративные компетенции">
-        <h1>Корпоративные компетенции</h1>
-      </tab>
-      <tab name="Профессиональные навыки">
-        <h1>Профессиональные навыки</h1>
-      </tab>
-      <tab name="Предварительное решение об итоговой оценке">
-        <h1>Предварительное решение об итоговой оценке</h1>
-      </tab>
-      <tab name="Завершение оценки">
-        <h1>Завершение оценки</h1>
-      </tab>
-    </tabs>
-    <button v-if="prevIsVisible" @click="prev()">Prev</button>
-    <button @click="next()">Next</button>
-  </div>
+    <div id="app">
+        <tabs ref="tabs" @indexChanged="indexChangedHandler">
+            <tab name="Корпоративные ценности" :selected="true">
+                <h1>Корпоративные ценности</h1>
+                <div>
+                    <p>Оцените, соответсвует ли поведение сотрудника орпторативным ценностям Х5</p>
+                    <div>
+                        <h2>1. Честность и порядочность</h2>
+                        <item
+                                title="Соблюдает договоренности"
+                                :values="values"
+                                v-model="selectedData.first"
+                        />
+                        <item
+                                title="Дает коллегам информацию"
+                                v-model="selectedData.second"
+                                :values="values"/>
+                        <item
+                                title="Абсолютно нетерпим к коррупции"
+                                v-model="selectedData.third"
+                                :values="values"/>
+                    </div>
+                </div>
+            </tab>
+            <tab name="Корпоративные компетенции">
+                <h1>Корпоративные компетенции</h1>
+            </tab>
+            <tab name="Профессиональные навыки">
+                <h1>Профессиональные навыки</h1>
+            </tab>
+            <tab name="Предварительное решение об итоговой оценке">
+                <h1>Предварительное решение об итоговой оценке</h1>
+            </tab>
+            <tab name="Завершение оценки">
+                <h1>Завершение оценки</h1>
+            </tab>
+        </tabs>
+        <button v-if="prevIsVisible" @click="prev()">Prev</button>
+        <button @click="next()">Next</button>
+        <button @click="save()">Сохранить</button>
+    </div>
 </template>
 
 <script>
-import Tabs from './components/Tabs.vue'
-import Tab from "./components/Tab";
+    import Tabs from './components/Tabs.vue'
+    import Tab from "./components/Tab";
+    import Item from "./components/item";
 
-export default {
-  name: 'App',
-  components: {
-    Tabs,
-    Tab
-  },
-  data(){
-    return {
-      selectedIndex: 0,
+    export default {
+        name: 'App',
+        components: {
+            Item,
+            Tabs,
+            Tab
+        },
+        data() {
+            return {
+                selectedIndex: 0,
+                selectedData: {
+                    'first': null,
+                    'second': null,
+                    'third': null
+                },
+                values: [
+                    {title: 'нет', value: '0'},
+                    {title: 'да, с исключениями', value: '1'},
+                    {title: 'да, полностью', value: '2'},
+                ],
+                saveInfo: {}
+            }
+        },
+        computed: {
+            prevIsVisible() {
+                return !!this.selectedIndex
+            }
+        },
+        methods: {
+            prev() {
+                this.$refs.tabs.prev()
+            },
+            next() {
+                this.$refs.tabs.next();
+                console.log(this.selectedData)
+            },
+            indexChangedHandler(index) {
+                this.selectedIndex = index
+            },
+            save() {
+                this.saveInfo = {...this.selectedData}
+                console.log(this.saveInfo)
+            }
+        }
     }
-  },
-  computed: {
-    prevIsVisible(){
-      return !!this.selectedIndex
-    }
-  },
-  methods: {
-    prev(){
-      this.$refs.tabs.prev()
-    },
-    next(){
-      this.$refs.tabs.next()
-    },
-    indexChangedHandler(index){
-      this.selectedIndex = index
-    }
-  }
-}
 </script>
 
 <style>
-  ul{
-    display: flex;
-    justify-content: space-around;
-  }
+    ul {
+        display: flex;
+        justify-content: space-around;
+    }
 </style>
---
+++ src/components/item.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>{{title}}</p>
+        <div v-for="value in values" :key="value.value">
+            <label>{{value.title}}</label>
+            <input type="radio" :value='value.value' :checked="value.value === selectedData" @change="valueChangeHandler">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "item",
+        props: ['title', 'selectedData', 'values'],
+        model: {
+            prop: 'selectedData',
+            event: 'valueChange'
+        },
+        methods: {
+            valueChangeHandler(e) {
+                const value = e.target.value;
+                this.$emit('valueChange', value);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
